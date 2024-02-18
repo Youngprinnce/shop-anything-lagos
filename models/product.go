3d62fc2837cd2045aacee19371bc366f9dfe3aad
youngprinnce <ajiboyeadedotun16@gmail.com>
2024-02-19 00:10:24 +0100

    Created the product model to represent the structure of products in the system.

--- models/product.go
+++ models/product.go
@@ -1,1 +1,18 @@
-package product
+package models
+
+import (
+	"time"
+)
+
+// Product represents a product in the shop
+type Product struct {
+    // Define product fields here
+    ID          string `json:"id,omitempty" gorm:"primary_key"`
+    MerchantID  string `json:"merchant_id" gorm:"index"`
+    SKU         string `json:"sku" gorm:"unique"`
+    Name        string `json:"name"`
+    Description string `json:"description"`
+    Price       float64 `json:"price"`
+    CreatedAt   time.Time `json:"created_at,omitempty" gorm:"created_at"`
+    UpdatedAt   time.Time `json:"updated_at,omitempty" gorm:"updated_at"`
+}
