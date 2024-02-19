02a354399ad37ed89f91ce36ce07e80aa8236bf1
youngprinnce <ajiboyeadedotun16@gmail.com>
2024-02-19 14:30:32 +0100

    Refactor code to include validation and clear EOF error

--- middleware/validation.go
+++ middleware/validation.go
@@ -1,7 +1,9 @@
 package middleware
 
 import (
+	"bytes"
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/Youngprinnce/shop-anything-lagos/models"
@@ -9,20 +11,36 @@ import (
 )
 
 func ValidateProduct(next http.HandlerFunc, validate func(models.Product) error) http.HandlerFunc {
-    return func(w http.ResponseWriter, r *http.Request) {
-        var product models.Product
-        err := json.NewDecoder(r.Body).Decode(&product)
-        if err != nil {
-            utils.HandleError(w, err, http.StatusBadRequest)
-            return
-        }
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Read the request body
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			utils.HandleError(w, err, http.StatusInternalServerError)
+			return
+		}
 
-        err = validate(product)
-        if err != nil {
-            utils.HandleError(w, err, http.StatusBadRequest)
-            return
-        }
+		// Restore the request body with the original data
+		r.Body = ioutil.NopCloser(bytes.NewBuffer(body))
 
-        next(w, r)
-    }
+		// Decode the request body into a Product struct for validation
+		var product models.Product
+		if err := json.Unmarshal(body, &product); err != nil {
+			utils.HandleError(w, err, http.StatusBadRequest)
+			return
+		}
+
+		// Validate the product
+		if err := validate(product); err != nil {
+			utils.HandleError(w, err, http.StatusBadRequest)
+			return
+		}
+
+		// Call the next handler
+		next(w, r)
+	}
 }
+
+
+// func NewError(message string) error {
+//     return errors.New(message)
+// }
--- services/product.go
+++ services/product.go
@@ -10,8 +10,8 @@ import (
 )
 
 var (
-    // Use a sync.Map for storing products with merchant ID as key and a slice of products as value
-    products = sync.Map{}
+	// Use a sync.Map for storing products with merchant ID as key and a slice of products as value
+	products = sync.Map{}
 )
 
 // CreateProduct creates a new product for a merchant.
@@ -20,15 +20,15 @@ func CreateProduct(merchantID string, product models.Product) (models.Product, e
 	merchantProducts, ok := products.Load(merchantID)
 	if !ok {
 		// If the merchant has no products, create a new slice
-	   	merchantProducts = []models.Product{}
+		merchantProducts = []models.Product{}
 	}
 
 	//  Check if the product sku already exists in the merchant's products
 	for _, merchantProduct := range merchantProducts.([]models.Product) {
-        if merchantProduct.SKU == product.SKU {
-            return models.Product{}, fmt.Errorf("product with SKU %s already exists for merchant %s", product.SKU, merchantID)
-        }
-    }
+		if merchantProduct.SKU == product.SKU {
+			return models.Product{}, fmt.Errorf("product with SKU %s already exists for merchant %s", product.SKU, merchantID)
+		}
+	}
 
 	// Generate a new product ID
 	productID, err := utils.GenerateProductID()
@@ -42,5 +42,15 @@ func CreateProduct(merchantID string, product models.Product) (models.Product, e
 
 	// Append the new product to the slice
 	products.Store(merchantID, append(merchantProducts.([]models.Product), product))
+
+	// console the content of the products to the console
+		// products.Range(func(key, value interface{}) bool {
+		// 	fmt.Printf("Merchant ID: %v\n", key)
+		// 	product := value.([]models.Product)
+		// 	for _, p := range product {
+		// 		fmt.Printf("Product: %v\n", p)
+		// 	}
+		// 	return true
+		// })
 	return product, nil
 }
