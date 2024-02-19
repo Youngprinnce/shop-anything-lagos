58b4617798980872dd02e4beb614a4f34cc0f3c8
youngprinnce <ajiboyeadedotun16@gmail.com>
2024-02-19 02:23:38 +0100

    Implement Validation middleware

---
+++ middleware/validation.go
@@ -0,0 +1,28 @@
+package middleware
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/Youngprinnce/shop-anything-lagos/models"
+	"github.com/Youngprinnce/shop-anything-lagos/utils"
+)
+
+func ValidateProduct(next http.HandlerFunc, validate func(models.Product) error) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        var product models.Product
+        err := json.NewDecoder(r.Body).Decode(&product)
+        if err != nil {
+            utils.HandleError(w, err)
+            return
+        }
+
+        err = validate(product)
+        if err != nil {
+            utils.HandleError(w, err)
+            return
+        }
+
+        next(w, r)
+    }
+}
